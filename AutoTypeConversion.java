f02dac2f7c78259011e78795ff42da1ede6e9d05
Hakashi Katake <gammlast@gmail.com>
2025-01-03 11:47:33 +0530

    automatic type conversion

---
+++ AutoTypeConversion.java
@@ -0,0 +1,13 @@
+//auto type conversion
+
+public class AutoTypeConversion {
+    public static void main(String[] args) {
+        int a = 100;
+        long b = a;
+        float c = b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+  
+    }
+}
