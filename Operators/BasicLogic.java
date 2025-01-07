e5ac695273729c09fa596db1c5670f308a615115
Hakashi Katake <gammlast@gmail.com>
2025-01-07 10:17:42 +0530

    Operators Assignment

---
+++ Operators/BasicLogic.java
@@ -0,0 +1,8 @@
+package Operators;
+
+public class BasicLogic {
+    //write a program to demonstrate logical operators
+    public static void main(String[] args) {
+        
+    }
+}
--- Operators/PrePostDec.java
+++ Operators/PrePostDec.java
@@ -1,9 +1,11 @@
 package Operators;
 
 public class PrePostDec {
-    //write a program to demonstrate increament and decrement operators
+    
     public static void main(String[] args) {
+        //write a program to demonstrate increament and decrement operators
         
+
     }
 
 }
