055ea96f6364c704c83f61a9a6ff07071c0ffe80
Hakashi Katake <gammlast@gmail.com>
2025-01-07 10:44:25 +0530

    Oppequals

--- Operators/OpEquals.java
+++ Operators/OpEquals.java
@@ -1,3 +1,9 @@
+package Operators;
 public class OpEquals {
-    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+    }
 }
